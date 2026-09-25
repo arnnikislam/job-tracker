58cb4c33eb75e03ba5bf4382e15847db1a427013
Payel <arnnikislam.socials@gmail.com>
2026-09-25 20:20:21 +0600

    added others requirements

--- js/script.js
+++ js/script.js
@@ -1,21 +1,78 @@
-// accessing every card data
-function jobData(cardId) {
-  // companyName, position, location, type, salary ,description
-  document.getElementById(cardId).style.display = "none";
-  const jobDataCopy = document.getElementById(cardId).innerHTML;
-  const jobDataCopyContainer = document.createElement("div");
-}
+// interview and rejected number count
+let interviewCount = 0,
+  rejectedCount = 0;
 
-// interview btn
+//   total jobs count
+const allJob = document.querySelectorAll(".card");
+let totalJobCount = allJob.length;
+document.querySelector(".total-count").innerText = totalJobCount;
+
+// accessing every card data and showing on other tab
 document
-  .querySelector(".interview-btn1")
-  .addEventListener("click", function () {
-    // console.log("btn1")
-    jobData("card1");
-  });
+  .querySelector(".job-cards-container")
+  .addEventListener("click", function (event) {
+    // for interview
+    if (event.target.classList.contains("interview-btn")) {
+      // take the card data
+      const card = event.target.closest(".card");
+      //   changing the status
+      card.querySelector(".status-btn").innerText = "INTERVIEW";
+      card.style.display = "none";
+      const cardDataCopy = document.createElement("div");
+      cardDataCopy.classList.add("card");
+      cardDataCopy.innerHTML = card.innerHTML;
+      document.querySelector(".interview-tab").append(cardDataCopy);
 
-//   interview tab
-document.querySelector(".interview-tab");
+      //   counting
+      document.querySelector(".interview-count").innerText = ++interviewCount;
+
+      // available jobs count
+      const availableJobs =
+        parseInt(totalJobCount) -
+        parseInt(interviewCount) -
+        parseInt(rejectedCount);
+      document.querySelector(".available-jobs").innerText = availableJobs;
+    }
+    // for rejected
+    if (event.target.classList.contains("rejected-btn")) {
+      // take the card data
+      const card = event.target.closest(".card");
+      //   changing the status
+      card.querySelector(".status-btn").innerText = "REJECTED";
+      card.style.display = "none";
+      const cardDataCopy = document.createElement("div");
+      cardDataCopy.classList.add("card");
+      cardDataCopy.innerHTML = card.innerHTML;
+      document.querySelector(".rejected-tab").append(cardDataCopy);
+
+      //   counting
+      document.querySelector(".rejected-count").innerText = ++rejectedCount;
+
+      // available jobs count
+      const availableJobs =
+        parseInt(totalJobCount) -
+        parseInt(interviewCount) -
+        parseInt(rejectedCount);
+      document.querySelector(".available-jobs").innerText = availableJobs;
+    }
+
+    // delete btn functionality
+    if (event.target.classList.contains("svg")) {
+      const parentCard = event.target.closest(".card");
+      parentCard.style.display = "none";
+
+      //   rest total
+      --totalJobCount;
+      document.querySelector(".total-count").innerText = totalJobCount;
+    }
+
+    // available jobs count
+    const availableJobs =
+      parseInt(totalJobCount) -
+      parseInt(interviewCount) -
+      parseInt(rejectedCount);
+    document.querySelector(".available-jobs").innerText = availableJobs;
+  });
 
 // toggling feature and btn active color features
 function toggleTab(tabClassName, tabBtnClassName) {
@@ -41,9 +98,31 @@ document
   .querySelector(".interview-tab-btn")
   .addEventListener("click", function () {
     toggleTab(".interview-tab", ".interview-tab-btn");
+
+    if (interviewCount === 0) {
+      const interviewTab = document.querySelector(".interview-tab");
+      if (!interviewTab.querySelector(".alert")) {
+        const copyAlert = document.createElement("div");
+        copyAlert.innerHTML =
+          document.querySelector(".alert-section").innerHTML;
+        document.querySelector(".interview-tab").append(copyAlert);
+        document.querySelector(".interview-tab").style.display = "block";
+      }
+    }
   });
 document
   .querySelector(".rejected-tab-btn")
   .addEventListener("click", function () {
     toggleTab(".rejected-tab", ".rejected-tab-btn");
+
+    if (rejectedCount === 0) {
+      const rejectedTab = document.querySelector(".rejected-tab");
+      if (!rejectedTab.querySelector(".alert")) {
+        const copyAlert = document.createElement("div");
+        copyAlert.innerHTML =
+          document.querySelector(".alert-section").innerHTML;
+        document.querySelector(".rejected-tab").append(copyAlert);
+        document.querySelector(".rejected-tab").style.display = "block";
+      }
+    }
   });
